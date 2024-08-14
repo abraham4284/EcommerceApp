de70add22105e7ed1e87d83723e8c03744276c43
Abraham <abraham4284@hotmail.com>
2024-08-14 02:09:43 -0300

    Cambio definitivo

--- client/src/context/AuthProvider.jsx
+++ client/src/context/AuthProvider.jsx
@@ -7,6 +7,7 @@ import {
   createUsuariosSuperAdminRequest,
   updateUsuariosRequest,
   getIdUsuariosRequest,
+  logoutRequest,
 } from "../Api/Autentificacion";
 import Cookies from "js-cookie";
 import { Spiner } from "../components/Spiner";
